72fb61ab7f5b82ee969c47f214c8eb9094e5c847
Frank Bao <xiang.bao.frank@gmail.com>
2017-01-09 15:38:19 +0800

    add login feature

--- MyEquipment.py
+++ MyEquipment.py
@@ -1,50 +1,81 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
 from werkzeug.security import check_password_hash, generate_password_hash
+from wtforms import PasswordField, BooleanField, SubmitField, StringField
+from wtforms.validators import DataRequired, Length
+from flask_migrate import MigrateCommand,Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '@b$p@th'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+login_manager = LoginManager(app)
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
 
 
-# Views
+# Routes
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/borrow_equipment')
+@app.route('/borrow_equipment', methods=['GET', 'POST'])
 def borrow():
     return render_template('borrow.html')
 
 
-@app.route('/return_equipment')
+@app.route('/return_equipment', methods=['GET', 'POST'])
 def return_equipment():
     return render_template('return.html')
 
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET', 'POST'])
+@login_required
 def admin():
     return render_template('admin.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(sso=form.sso.data).first()
+        if user is not None and user.verify_password(form.pwd.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('Next') or url_for('index'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You've been logged out.")
+    return redirect(url_for('index'))
+
+
 # Models
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     sso = db.Column(db.Integer, primary_key=True)
     last_name = db.Column(db.String(64), nullable=False)
     first_name = db.Column(db.String(64), nullable=False)
     is_active = db.Column(db.Boolean, nullable=False, default=True)
-    records = db.relationship('Record', backref='sso')
+    # records = db.relationship('Record', backref='sso')
     password_hash = db.Column(db.String(128))
 
     @property
@@ -63,7 +94,7 @@ class Equipment(db.Model):
     __tablename__ = 'equipments'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    records = db.relationship('Record', backref='equipment')
+    # records = db.relationship('Record', backref='equipment')
 
 
 class Record(db.Model):
@@ -72,6 +103,19 @@ class Record(db.Model):
     sso = db.Column(db.Integer, db.ForeignKey('users.sso'))
     equipment = db.Column(db.Integer, db.ForeignKey('equipments.id'))
     record_type = db.Column(db.String, nullable=False)
+
+
+@login_manager.user_loader
+def load_user(sso):
+    return User.query.get(int(sso))
+
+
+# Forms
+class LoginForm(FlaskForm):
+    sso = StringField("SSO:", validators=[DataRequired(), Length(9, 9)])
+    pwd = PasswordField("Password:", validators=[DataRequired()])
+    remember_me = BooleanField("Keep me logged in.")
+    submit = SubmitField('Log In')
 
 
 if __name__ == '__main__':
